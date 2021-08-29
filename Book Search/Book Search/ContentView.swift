d1bb26b33dca5c64add5da234de0956c4be0c901
adrianorezena <adrianorezena@users.noreply.github.com>
2021-08-28 23:00:59 -0300

    project started

---
+++ Book Search/Book Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Book Search
+//
+//  Created by Adriano Rezena on 28/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
